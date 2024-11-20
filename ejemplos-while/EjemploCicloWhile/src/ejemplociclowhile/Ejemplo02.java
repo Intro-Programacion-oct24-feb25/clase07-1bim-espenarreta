69ee3c2b76efc6cb0736b0fa156d579ee60b90f2
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-11-20 11:59:50 -0500

    Cambios

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo02.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo02.java
@@ -17,14 +17,17 @@ public class Ejemplo02 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
-        entrada.useLocale(Locale.US);
+        entrada.useLocale(Locale.US); // El use.locate es para que todos los
+                                      //decimales se ingresen con punto(.)
         int limite;
         int contador = 1;
         System.out.println("Ingrese el Límite");
         limite = entrada.nextInt(); 
         while (contador <= limite){
             
-            if (contador%2==0) {
+            if (contador%2!=0) { // El MOD sirve para sacar el resiuo de los
+                                 // número(Si el número es par el residuo es 0
+                                 // si es impar es 1)
                 System.out.printf("Contador %d\n", 
                     contador);
             }
