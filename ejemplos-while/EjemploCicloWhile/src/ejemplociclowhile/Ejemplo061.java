7c0ab03741a6ef94bb24219cfd34206404c2d58a
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-11-21 10:36:53 -0500

    Cambios

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo061.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo061.java
@@ -37,7 +37,12 @@ public class Ejemplo061 {
 
             if (calificacion > 20) {
                 calificacion = 20;
+            } else {
+                if (calificacion >= 0 && calificacion <= 5){
+                    calificacion = 6;                    
+                }
             }
+            
             suma_total = suma_total + calificacion;
 
             // proceso para salir del ciclo.
